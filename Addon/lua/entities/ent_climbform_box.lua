ff7cf6d9b5547138f31eb0ce77fbb44848beeadd
Zeni <timrace01@gmail.com>
2019-09-23 02:29:29 +0200

    Ran through glualinter Massively reduced scope pyramids Deleted C style Removed unnecessary if conditions Scream if anything is broken

--- Addon/lua/entities/ent_climbform_box.lua
+++ Addon/lua/entities/ent_climbform_box.lua
@@ -1,25 +1,21 @@
 --Made by MrRangerLP
-
 --Shared
 AddCSLuaFile()
-
-ENT.Base				= "base_gmodentity"
-ENT.Type				= "anim"
- 
-ENT.PrintName			= "Climbgame Box"
-ENT.Category			= "Fun + Games"
-ENT.Author				= "MrRangerLP"
-ENT.Contact				= ""
-ENT.Purpose				= ""
-ENT.Instructions		= ""
-ENT.Spawnable			= false
-ENT.AdminOnly			= false
-ENT.DisableDuplicator	= true
+ENT.Base = "base_gmodentity"
+ENT.Type = "anim"
+ENT.PrintName = "Climbgame Box"
+ENT.Category = "Fun + Games"
+ENT.Author = "MrRangerLP"
+ENT.Contact = ""
+ENT.Purpose = ""
+ENT.Instructions = ""
+ENT.Spawnable = false
+ENT.AdminOnly = false
+ENT.DisableDuplicator = true
 
 if SERVER then
 	function ENT:Initialize()
 		self:SetModel("models/props_junk/wood_crate001a.mdl")
-		
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetMoveType(MOVETYPE_VPHYSICS)
 		self:SetSolid(SOLID_VPHYSICS)
@@ -29,35 +25,51 @@ if SERVER then
 			phys:Wake()
 			phys:EnableMotion(false)
 		end
-		
+
+		local plyValid = IsValid(self.Owner)
 		if self.CPPIExists then
-			if self.Owner then
-				if self.Owner:IsValid() then
-					self:CPPISetOwner(self.Owner)
-				end
+			if plyValid then
+				self:CPPISetOwner(self.Owner)
 			end
 		else
-			if self.Owner then
-				if self.Owner:IsValid() then
-					self:SetNWEntity("my_owner",self.Owner) --TinyCPPI Compatibility
-				end
+			if plyValid then
+				self:SetNWEntity("my_owner", self.Owner) --TinyCPPI Compatibility
 			end
 		end
 	end
-	
-	function ENT:EntityTakeDamage() return true end
-	function ENT:PhysgunPickup() return false end
-	function ENT:CanPlayerUnfreeze() return false end
-	function ENT:CanTool() return false end
+
+	function ENT:EntityTakeDamage()
+		return true
+	end
+
+	function ENT:PhysgunPickup()
+		return false
+	end
+
+	function ENT:CanPlayerUnfreeze()
+		return false
+	end
+
+	function ENT:CanTool()
+		return false
+	end
 end
 
 if CLIENT then
-	function ENT:Initialize() end
-	function ENT:OnRemove() end
-	function ENT:Draw() self.BaseClass.Draw(self) end
-	function ENT:Think() end
-	
+	function ENT:Initialize()
+	end
+
 	function ENT:OnRemove()
-		self:EmitSound("physics/wood/wood_crate_break"..math.random(1,5)..".wav",75,100,1,CHAN_AUTO)
+	end
+
+	function ENT:Draw()
+		self.BaseClass.Draw(self)
+	end
+
+	function ENT:Think()
+	end
+
+	function ENT:OnRemove()
+		self:EmitSound("physics/wood/wood_crate_break" .. math.random(1, 5) .. ".wav", 75, 100, 1, CHAN_AUTO)
 	end
 end
